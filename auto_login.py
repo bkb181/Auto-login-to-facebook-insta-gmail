09d0cc8c1b9fcdd237fe0b956fbcf6df817d0934
bkb181 <32813552+bkb181@users.noreply.github.com>
2019-04-12 22:39:04 +0530

    Update auto_login.py

--- auto_login.py
+++ auto_login.py
@@ -1,16 +1,11 @@
 from selenium import webdriver
-#browser = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\chromedriver.exe")
-browser = webdriver.Chrome(executable_path='D:\TheSurgicalStrike\chromedriver_win32/chromedriver.exe')
+browser = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\chromedriver.exe")
 
 browser.maximize_window()
-username = "useid"
-password = "pass"
-browser.get("https://www.facebook.com")
+username = "useid" # write your used id
+password = "pass"   # write your password
+browser.get("https://www.facebook.com")  # change the url for different website
 browser.implicitly_wait(3)
 browser.find_element_by_xpath('//*[@id="email"]').send_keys(username)
 browser.find_element_by_xpath('//*[@id="pass"]').send_keys(password)
-#How webpage looks before login screenshot
-browser.get_screenshot_as_file("Facebook.png")
-browser.find_element_by_id("loginbutton").click()
-#How webpage looks after login screenshot
-browser.get_screenshot_as_file("Facebook1.png")
+
